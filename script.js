c867d955cc66fb3daccc685826c41e9b3ea91380
Travis Jankowski <torist2@protonmail.com>
2021-11-15 09:10:11 -0600

    tweaked conditionals

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function updateOutput(inputs, outputs) { // update the screen with the device
     // determine output
-    if (inputs.screen[0].value >= 12 && (inputs.os[0].checked || inputs.os[1].checked || inputs.os[2].checked) && inputs.input[1].checked && !inputs.portable[0].checked) {
+    if (inputs.screen[0].value >= 12 && (inputs.os[0].checked || inputs.os[1].checked || inputs.os[2].checked) && !inputs.input[0].checked && inputs.input[1].checked && !inputs.portable[0].checked) {
         outputs.output.innerHTML = "Looks like you could use a desktop.";
         outputs.outputImage.src = "images/desktop.png";
-    } else if (inputs.screen[0].value <= 20 && inputs.screen[0].value >= 15 && (inputs.os[0].checked || inputs.os[1].checked || inputs.os[2].checked)) {
+    } else if (inputs.screen[0].value <= 20 && inputs.screen[0].value >= 15 && (inputs.os[0].checked || inputs.os[1].checked || inputs.os[2].checked) && inputs.input[1].checked) {
         outputs.output.innerHTML = "You could probably use a laptop.";
         outputs.outputImage.src = "images/laptop.png";
-    } else if (inputs.screen[0].value <= 15 && inputs.screen[0].value >= 9 && (inputs.os[0].checked || inputs.os[3].checked || inputs.os[4].checked) && inputs.portable[0].checked) {
+    } else if (inputs.screen[0].value <= 15 && inputs.screen[0].value >= 9 && (inputs.os[0].checked || inputs.os[3].checked || inputs.os[4].checked) && inputs.input[0].checked && inputs.portable[0].checked) {
         outputs.output.innerHTML = "You should get a tablet.";
         outputs.outputImage.src = "images/tablet.png";
-    } else if (inputs.screen[0].value <= 8 && (inputs.os[0].checked || inputs.os[3].checked || inputs.os[4].checked) && inputs.input[0].checked && inputs.portable[0].checked) {
+    } else if (inputs.screen[0].value <= 8 && (inputs.os[0].checked || inputs.os[3].checked || inputs.os[4].checked) && inputs.input[0].checked && !inputs.input[1].checked && inputs.portable[0].checked) {
         outputs.output.innerHTML = "Seems like you need a phone.";
         outputs.outputImage.src = "images/phone.png";
     } else {
