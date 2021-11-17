7bed4aeed1efde6934c2ef839d23b896b501e79d
Travis Jankowski <torist2@protonmail.com>
2021-11-17 13:09:25 -0600

    documentation

--- script.js
+++ script.js
@@ -3,18 +3,23 @@ function updateOutput(inputs, outputs) { // update the screen with the device
     outputs.screenOutput.innerHTML = inputs.screen[0].value + '"';
     // determine output
     if (inputs.screen[0].value >= 12 && (inputs.os[0].checked || inputs.os[1].checked || inputs.os[2].checked) && !inputs.input[0].checked && inputs.input[1].checked && !inputs.portable[0].checked) {
+        // desktop result
         outputs.output.innerHTML = "Looks like you could use a desktop.";
         outputs.outputImage.src = "images/desktop.png";
     } else if (inputs.screen[0].value <= 20 && inputs.screen[0].value >= 15 && (inputs.os[0].checked || inputs.os[1].checked || inputs.os[2].checked) && inputs.input[1].checked) {
+        // laptop result
         outputs.output.innerHTML = "You could probably use a laptop.";
         outputs.outputImage.src = "images/laptop.png";
     } else if (inputs.screen[0].value <= 15 && inputs.screen[0].value >= 9 && (inputs.os[0].checked || inputs.os[3].checked || inputs.os[4].checked) && inputs.input[0].checked && inputs.portable[0].checked) {
+        // tablet result
         outputs.output.innerHTML = "You should get a tablet.";
         outputs.outputImage.src = "images/tablet.png";
     } else if (inputs.screen[0].value <= 8 && (inputs.os[0].checked || inputs.os[3].checked || inputs.os[4].checked) && inputs.input[0].checked && !inputs.input[1].checked && inputs.portable[0].checked) {
+        // phone result
         outputs.output.innerHTML = "Seems like you need a phone.";
         outputs.outputImage.src = "images/phone.png";
     } else {
+        // no result
         outputs.output.innerHTML = "Pick your criteria to get a device.";
         outputs.outputImage.src = "images/pick.png";
     }
